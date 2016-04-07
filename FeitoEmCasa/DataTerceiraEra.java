b5efcec25356cc1bfd40f5a7a17a9aa47724d69b
Douglas Ballester <douglas.ballester@cwi.com>
2016-04-07 11:48:40 -0300

    refactor(DataTerceiraEra): utilizar apenas um return para calcular ano bissexto

--- FeitoEmCasa/DataTerceiraEra.java
+++ FeitoEmCasa/DataTerceiraEra.java
@@ -12,12 +12,7 @@ public class DataTerceiraEra{
     
     
     public boolean ehBissexto(){
-        if(ano % 400 == 0){
-            return true;
-        }else if(ano % 4 == 0 && ano % 100 != 0){
-            return true;
-        }else{
-            return false;
-        }
+        return(ano % 400 == 0 || ano % 4 == 0 && ano % 100 != 0);
+
     }
 }
