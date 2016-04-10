c91ca1721b23874454440a87d9383305e250a8f9
Douglas Ballester <douglas.ballester@cwi.com>
2016-04-09 23:20:58 -0300

    feat(Inventario): adicionar metodo getMaisPopular e getDescricaoItens test(InventarioTest):  testes referentes ao getDescricaoItens e colocar1000Unidades

--- FeitoEmCasa/Inventario.java
+++ FeitoEmCasa/Inventario.java
@@ -19,5 +19,37 @@ public class Inventario{
         return itens;
     }
     
+    public String getDescricaoItens(){
+        String listarItens = "";
+        for(int i = 0; i < itens.size(); i++){
+            listarItens += itens.get(i).getDescricao() + "," ;
+        }   
+        
+        return !itens.isEmpty() ? listarItens.substring(0, listarItens.length() - 1) : listarItens;
+    }
+    
+    public void ganhar1000UnidadesDeCadaItem(){
+        for(int i=0; i < itens.size(); i++){
+            itens.get(i).ganhar1000Unidades();
+        }
+        
+    }
+    
+    public Item getItemMaisPopular(){
+        int indice = 0 ,maiorQtd = 0;
+        for (int i = 0; i < itens.size(); i++){
+            int qtdAtual = itens.get(i).getQuantidade();
+            if (qtdAtual > maiorQtd){
+                maiorQtd = qtdAtual;
+                indice = i;
+            }
+        }
+        
+        return !this.itens.isEmpty() ? this.itens.get(indice) : null;
+        /*retorna null se não tiver item. Para evitar que quando o metodo seja
+        chamado aponte para uma posicao que nao exista ? Perguntar*/ 
+    }
+    
+    
    
 }
--- FeitoEmCasa/InventarioTest.java
+++ FeitoEmCasa/InventarioTest.java
@@ -3,29 +3,91 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class InventarioTest
 {
-    
-     @Test
-    public void AdicionaItemAoIventario(){
-        Item teste = new Item(1, "batata");
+    @Test
+    public void getDescricaoItensSemItem(){
         Inventario bag = new Inventario();
-        bag.adicionarItem(teste);
-        assertTrue(bag.getItens().size() == 1);
+     
+        
+        
+        String obtido = bag.getDescricaoItens();
+        assertEquals("", obtido);
     }
     
     
     
     
     @Test
-    public void RemoverItemDoInventario(){
-        Item teste = new Item(2, "Cenouras");
+    public void getDescricaoItensComUmItem(){
         Inventario bag = new Inventario();
+        Item teste = new Item(1,"Couve");
         bag.adicionarItem(teste);
-        assertTrue(bag.getItens().size() == 1);
-        bag.removerItem(teste);
-        assertTrue(bag.getItens().size()== 0);
+        
+        String obtido = bag.getDescricaoItens();
+        assertEquals("Couve", obtido);
     }
     
-}
+    
+    @Test
+    public void getDescricaoItensComTresItem(){
+        Inventario bag = new Inventario();       
+        Item item2 = new Item(1,"Off-hand");
+        Item item3 = new Item(1,"head");
+        bag.adicionarItem(new Item(1,"Staff")); //cria o item direto dentro  
+        bag.adicionarItem(item2);                //da bag(Inventario)
+        bag.adicionarItem(item3);
+        
+        String obtido = bag.getDescricaoItens();
+        assertTrue(obtido.equalsIgnoreCase("sTAfF,OfF-haNd,hEAd"));
+    }
+    
+    
+    @Test//Com inventario vazio
+    public void colocar1000UnidadesSemTerItens(){
+        Inventario bag = new Inventario();
+        bag.ganhar1000UnidadesDeCadaItem();
+   
+        assertTrue(bag.getItens().isEmpty());
+    }
+    
+    @Test
+    public void colocar1000UnidadesCom1Item(){
+        Inventario bag = new Inventario();
+        
+        bag.adicionarItem(new Item(5, "bows"));
+        bag.ganhar1000UnidadesDeCadaItem();
+   
+        assertEquals(1005, bag.getItens().get(0).getQuantidade());
+    }
+    
+    @Test
+    public void colocar1000UnidadesCom10Itens(){
+        Inventario bag = new Inventario();
+        
+        for(int i=0; i < 10 ; i++){
+            bag.adicionarItem(new Item(2, "bows"));
+            bag.ganhar1000UnidadesDeCadaItem();
+            assertEquals(1002, bag.getItens().get(i).getQuantidade());
+        }
+    }   
+        
+     @Test
+        public void colocar1000UnidadesCom2Itens(){
+            Inventario bag = new Inventario();
+            bag.adicionarItem(new Item (2, "Hammers"));
+            bag.ganhar1000UnidadesDeCadaItem();
+            assertEquals(1002, bag.getItens().get(0).getQuantidade());
+            bag.adicionarItem(new Item(15, "Arrows"));
+            bag.ganhar1000UnidadesDeCadaItem();
+            assertEquals(1015, bag.getItens().get(1).getQuantidade());
+            
+            
+        }
+        
+    }
+    
+    
+    
+    
+
