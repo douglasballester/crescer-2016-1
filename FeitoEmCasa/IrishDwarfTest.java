bebc86f4fcf8a8606c02b4811ba904090a37cb0e
Douglas Ballester <douglas.ballester@cwi.com>
2016-04-11 00:52:27 -0300

    feat(IrishDwarf):  criar IrishDwarf e adicionar metodo tentarSorte() test(IrishDwarfTest): criar cenarios para aumentar inventario com 1 item, 2 itens ou nenhum item

---
+++ FeitoEmCasa/IrishDwarfTest.java
@@ -0,0 +1,67 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class IrishDwarfTest{
+    
+    @Test
+    public void irishDwarfSortudoGanha6000UnidadesDeItem(){
+        Item item = new Item(3, "batatas");
+        DataTerceiraEra data = new DataTerceiraEra(1,1,1992);
+        IrishDwarf jr = new IrishDwarf("Gim" , data);
+        
+        jr.perdeVida();
+        jr.perdeVida();
+        jr.perdeVida();
+        
+        jr.adicionarItem(item);
+        jr.tentarSorte();
+        
+       assertEquals(-3333.0, jr.getNumeroSorte(), 0.1);
+       assertEquals(6003, jr.getInventario().getItens().get(0).getQuantidade());
+        
+    }
+    
+    @Test
+    public void irishDwarfSortudoGanhaMuitasUnidadesDeVariosItens(){
+        Item item = new Item(3, "batatas");
+        Item item1 = new Item(2, "Couves");
+        DataTerceiraEra data = new DataTerceiraEra(1,1,1992);
+        IrishDwarf jr = new IrishDwarf("Gim" , data);
+        
+        jr.perdeVida();
+        jr.perdeVida();
+        jr.perdeVida();
+        
+        jr.adicionarItem(item);
+        jr.adicionarItem(item1);
+        jr.tentarSorte();
+        
+       assertEquals(-3333.0, jr.getNumeroSorte(), 0.1);
+       assertEquals(6003, jr.getInventario().getItens().get(0).getQuantidade());
+       assertEquals(3002, jr.getInventario().getItens().get(1).getQuantidade());
+        
+    }
+    
+    @Test
+     public void irishDwarfSortudoGanhaMuitasUnidadesComInventarioVazio()
+     {
+         IrishDwarf jr = new IrishDwarf("HMM", new DataTerceiraEra(1,1,1992));
+         
+         jr.perdeVida();
+         jr.perdeVida();
+         
+         
+         jr.tentarSorte();
+         
+         assertTrue(jr.getInventario().getItens().isEmpty());
+    }   
+    
+    
+  
+}
+   
