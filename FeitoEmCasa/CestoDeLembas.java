b8c4fdbb06eebe028638b82cdfb9b748787c5d71
Douglas Valle Ballester <douglas.ballester@cwinet.local>
2016-04-12 14:20:04 -0300

    refactor(CestoDeLembas): excluir codigo sem utilidade

--- FeitoEmCasa/CestoDeLembas.java
+++ FeitoEmCasa/CestoDeLembas.java
@@ -1,10 +1,3 @@
-
-/**
- * Write a description of class CestoDeLembas here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class CestoDeLembas
 {
     private int numeroDeLembas;
