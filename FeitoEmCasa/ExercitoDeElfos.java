6e46679997f7bc906f77153232cc305db9ba81b9
Douglas Ballester <douglas.ballester@cwi.com>
2016-04-14 01:14:26 -0300

    feat(ExercitoDeElfos): criar classe ExercitoDeElfos test(ExercitoDeElfosTest):

--- FeitoEmCasa/ExercitoDeElfos.java
+++ FeitoEmCasa/ExercitoDeElfos.java
@@ -2,24 +2,37 @@ import java.util.*;
 public class ExercitoDeElfos
 {
     private HashMap<String, Elfo> exercito = new HashMap<>();
+    private HashMap<Status, ArrayList<Elfo>> elfosAgrupadosPorStatus = new HashMap<>();
     
     public void alistarElfo(Elfo elfo){
-        //Aceitar somente Elfos Verdes e Elfos Noturnos
         if(elfo instanceof ElfoVerde || elfo instanceof ElfoNoturno){
             exercito.put(elfo.getNome(), elfo);
         }
     }
     
-    public Elfo buscarElfo(String nome){
-        for (String chave : exercito.keySet()){
-           if (chave.equals(nome)){
-               return exercito.get(chave);
-            }
-        }
-        return null;
+    public Elfo buscarPorNome(String nome){
+        return exercito.get(nome);
     }
     
     public HashMap getExercitoDeElfos(){
         return exercito;
+    }
+    
+    public void agruparPorStatus(){
+        for (Elfo elfo : exercito.values()){
+            if (!elfosAgrupadosPorStatus.containsKey(elfo.getStatus())){
+                elfosAgrupadosPorStatus.put(elfo.getStatus(), new ArrayList<Elfo>()); 
+            }
+                elfosAgrupadosPorStatus.get(elfo.getStatus()).add(elfo);
+            
+        }
+    }
+    
+    public HashMap getExercitoAgrupado(){
+        return elfosAgrupadosPorStatus;
+    }
+    
+      public ArrayList<Elfo> buscarPorStatus(Status status){
+        return elfosAgrupadosPorStatus.get(status);
     }
 }
--- FeitoEmCasa/ExercitoDeElfosTest.java
+++ FeitoEmCasa/ExercitoDeElfosTest.java
@@ -2,17 +2,69 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
+import java.util.*;
 
 public class ExercitoDeElfosTest
 {    
     @Test
-    public void InvitarElfoNormal(){
-        ExercitoDeElfos teste = new ExercitoDeElfos();
+    public void invitarElfoNormal(){
+        ExercitoDeElfos army = new ExercitoDeElfos();
         Elfo e1 = new Elfo("haldir");
         
-       teste.alistarElfo(e1);
+       army.alistarElfo(e1);
        
-       assertEquals(false, teste.getExercitoDeElfos().containsValue(e1));
+       assertEquals(false, army.getExercitoDeElfos().containsValue(e1));
+    }
+    
+    @Test
+    public void invitarElfoVerdeENoturno(){
+       ExercitoDeElfos army = new ExercitoDeElfos();
+       Elfo e1 = new ElfoVerde("haldir");
+       Elfo e2 = new ElfoNoturno("legolas");
+        
+       army.alistarElfo(e1);
+       army.alistarElfo(e2);
+       assertEquals(true, army.getExercitoDeElfos().containsValue(e1));
+       assertEquals(true, army.getExercitoDeElfos().containsValue(e2));
+    }
+    
+    @Test
+    public void invitarElfoNormalComElfoNoturno(){
+       ExercitoDeElfos army = new ExercitoDeElfos();
+       Elfo e1 = new Elfo("haldir");
+       Elfo e2 = new ElfoNoturno("legolas");
+        
+       army.alistarElfo(e1);
+       army.alistarElfo(e2);
+       assertEquals(false, army.getExercitoDeElfos().containsValue(e1));
+       assertEquals(true, army.getExercitoDeElfos().containsValue(e2));
+    } 
+    
+    @Test
+    public void buscarElfoPorNome(){
+        // busca elfo por nome e retorna objeto elfo.
+        ExercitoDeElfos army = new ExercitoDeElfos();
+        Elfo e1 = new ElfoNoturno("Elrond");
+        
+        army.alistarElfo(e1);
+        
+        assertEquals(e1,army.buscarPorNome("Elrond"));
+    }
+    
+    @Test
+    public void exercitoAgrupadoCom1MortoE1Vivo(){
+        Elfo elf1 = new ElfoNoturno("anastacia");
+        Elfo elf2 = new ElfoVerde("robin hood");
+        do{
+            elf1.atirarFlecha(new Dwarf("alvo"));
+        }while(elf1.getStatus() == Status.VIVO);
+        ExercitoDeElfos army = new ExercitoDeElfos();
+        army.alistarElfo(elf1);
+        army.alistarElfo(elf2);        
+       
+        army.agruparPorStatus();
+        
+        assertTrue(army.getExercitoAgrupado().containsKey(Status.VIVO) &&
+                   army.getExercitoAgrupado().containsKey(Status.MORTO));
     }
 }
