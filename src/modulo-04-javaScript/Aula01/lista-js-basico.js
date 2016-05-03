ac4267f5f908ec470f9fb0103b82531b2deaeba0
Douglas Valle Ballester <douglas.ballester@cwinet.local>
2016-05-03 15:38:56 -0300

    feat(exerciciosAula01): adicionar exercicios aula 01 test(exerciciosAula01): criar cenarios de testes para os exercicios

--- src/modulo-04-javaScript/Aula01/lista-js-basico.js
+++ src/modulo-04-javaScript/Aula01/lista-js-basico.js
@@ -1,29 +1,92 @@
 'use strict'
+//Exercicio 01
 var daisyGame = function(nPetalas){
-    if(nPetalas % 2 === 0){
-      return console.log('love me not');
+
+    return nPetalas % 2 === 0 ? 'Love me not' : 'Love me';
+
+}
+//----------------------------------------------------------
+//Exercicio 02
+function maiorTexto(palavras) {
+  var maiorPalavra = '';
+  for(var i=0; i < palavras.length; i++ ){
+    if (maiorPalavra.length < palavras[i].length)
+      maiorPalavra = palavras[i];
+  }
+    return(maiorPalavra);
+}
+//-------------------------------------------------------------
+//Exercicio 03
+var imprime = function(arrayString,fn)
+{
+    if(typeof fn === 'function'){
+      for(var i = 0;  i < arrayString.length; i++){
+        fn(arrayString[i]);
+      }
     }
-    else{
-      return console.log('love me');};
+}
+//--------------------------------------------------------------
+
+//Exercicio 04
+
+function adicionar(valor1){
+  return function(valor2){
+    return valor1 + valor2;
+  }
+}
+
+//--------------------------------------------------------------
+//Exercicio 05
+var fiboSum = function(n, usarRecursao) {
+
+  if (usarRecursao) {
+    if (n === 1) return 1;
+    return fibonacci(n, true) + fiboSum(n-1, true);
+  }
+
+  var calcularIterativo = function() {
+    var anterior = 0, atual = 1, soma = 0, prox;
+    for (var i = 0; i < n; i++) {
+      prox = anterior + atual;
+      soma += atual;
+      anterior = atual;
+      atual = prox;
+    }
+    return soma;
   };
 
-var bM = daisyGame(4);
-bM = daisyGame(3);
+  return calcularIterativo();
 
-var maiorTexto = function(palavras){
-  var varMaiorPalavra;
-  var ndexMaiorPalavra;
-    for (var i = 0; i < palavras.length; i++) {
-      if(i = 0){
-        varMaiorPalavra = palavras[i];
-         ndexMaiorPalavra = i;
-      }
-      if(varMaiorPalavra < palavras[i]){
-        varMaiorPalavra = palavras[i];
-        indexMaiorPalavra = i;
-      }
-      return console.log(varMaiorPalavra);
-    }
 };
+//--------------------------------------------------------------
+//Exercicio 06
+function queroCafe(mascada, precos){
+  if(precos.constructor === Array){
+    for(var i = 0; i < precos.length; i++){
+      if(mascada < precos[i]){
+        precos.splice(i, 1);
+        i--;
+      }
+    }
+    return precos.sort().toString();
+  }
+}
 
-maiorTexto('oi', 'teste', 'sei la', '1', '33');
+
+//Exercicio 07
+function contarPorTipo (objeto, tipo) { // REVER
+
+  // inspirado em http://stackoverflow.com/a/13467007/5194966
+  function getType(v) {
+    return v === null ? 'null' : typeof v !== 'undefined' && v.constructor === Array ? 'array' : typeof v;
+  }
+
+  var count = 0;
+  for (var campo in objeto) {
+    if (getType(objeto[campo]) === tipo) count++;
+  }
+
+  return count;
+}
+
+//Exercicio 08
