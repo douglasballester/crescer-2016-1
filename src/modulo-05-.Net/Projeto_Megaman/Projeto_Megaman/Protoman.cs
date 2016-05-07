57a7075eaec01d42f2cd1c84f6b7e2b86404f711
Douglas Ballester <douglas.ballester@cwi.com>
2016-05-07 04:38:17 -0300

    feat(Protoman): criar classe protoman

---
+++ src/modulo-05-.Net/Projeto_Megaman/Projeto_Megaman/Protoman.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Megaman
+{
+    public class Protoman : Robo
+    {
+        
+        public Protoman()
+        {
+           
+        }
+
+        protected override int Ataque
+        {
+            get
+            {
+                return Ataque;
+            }
+        }
+
+        protected override int Defesa
+        {
+            get
+            {
+                return 2;
+            }
+        }
+
+        public override void Atacar(Robo robo)
+        {
+            base.Atacar(robo);
+        }
+
+        public override void ReceberAtaque(int ataque)
+        {
+            base.ReceberAtaque(ataque);
+            int contMorte = 0;
+            if(Vida <= 0 && contMorte == 0)
+            {
+                Vida = 20;
+                contMorte = 1;
+            }
+        }
+
+    }
+}
