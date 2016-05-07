00ccee0ffc93c88da72392d5848750d2c41c9673
Douglas Ballester <douglas.ballester@cwi.com>
2016-05-07 04:35:49 -0300

    feat(Megaman): criar classe megaman test(MegamanTest): criado cenarios para classe Megaman

---
+++ src/modulo-05-.Net/Projeto_Megaman/Projeto_Megaman/Megaman.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Megaman
+{
+    public class Megaman : Robo
+    {
+        public Megaman() : base()
+        {
+        }
+
+        protected override int Ataque
+        {
+            get
+            {
+                return 6;
+            }
+        }
+
+        public override void Atacar(Robo robo)
+        {
+            if (Vida < 30)
+            {
+                int dano = this.Ataque + 3;
+                robo.ReceberAtaque(dano);
+            }
+            else
+            {
+                robo.ReceberAtaque(this.Ataque);
+            }
+        }
+
+        public override void ReceberAtaque(int ataque)
+        {
+            base.ReceberAtaque(ataque);
+        }
+    }
+}
---
+++ src/modulo-05-.Net/Projeto_Megaman/Projeto_Megaman_Test/Projeto_Megaman_Test/MegamanTest.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Projeto_Megaman;
+
+namespace Projeto_Megaman_Test
+{
+    [TestClass]
+    public class MegamanTest
+    {
+        [TestMethod]
+        public void CriarMegaman()
+        {
+            Megaman megaman = new Megaman();
+
+            Assert.AreEqual(100, megaman.Vida);
+        }
+
+       [TestMethod]
+        public void MegamanAtacaBot()
+        {
+            Robo megaman = new Megaman();
+            Robo bot = new Bot();
+
+            megaman.Atacar(bot);
+
+            Assert.AreEqual(94, bot.Vida);
+        }
+
+        [TestMethod]
+        public void MegamanAtacaBot10Vezes()
+        {
+            Robo megaman = new Megaman();
+            Robo bot = new Bot();
+
+            for (int i = 0; i < 10; i++)
+            {
+                megaman.Atacar(bot);
+            }
+            Assert.AreEqual(40, bot.Vida);
+        }
+
+        [TestMethod]
+        public void MegamanAtacaBotComMenosDe30Vida()
+        {
+            Robo megaman = new Megaman();
+            Robo bot = new Bot();
+
+            megaman.ReceberAtaque(71);
+
+            megaman.Atacar(bot);
+
+            Assert.AreEqual(29, megaman.Vida);
+           Assert.AreEqual(91, bot.Vida);
+        }
+
+        [TestMethod]
+        public void MegamanAtacaProtomanComMenosDe30Vida()
+        {
+            Robo megaman = new Megaman();
+            Robo protoman = new Protoman();
+
+            megaman.ReceberAtaque(71);
+
+            megaman.Atacar(protoman);
+
+            Assert.AreEqual(93, protoman.Vida);
+        }
+
+        [TestMethod]
+        public void MegamanAtacaProtoman()
+        {
+            Robo megaman = new Megaman();
+            Robo protoman = new Protoman();
+
+            megaman.Atacar(protoman);
+
+            Assert.AreEqual(96, protoman.Vida);
+        }
+
+        [TestMethod]
+        public void BotAtacaMegaman()
+        {
+            Robo megaman = new Megaman();
+            Robo bot = new Bot();
+
+            bot.Atacar(megaman);
+
+            Assert.AreEqual(95, megaman.Vida);
+        }
+
+    }
+}
