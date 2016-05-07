30abfe4c28029fdb5b6cd4b7e176bb4b8a74de98
Douglas Ballester <douglas.ballester@cwi.com>
2016-05-07 04:36:31 -0300

    feat(Robo, Bot): criar classes Robo e Bot

---
+++ src/modulo-05-.Net/Projeto_Megaman/Projeto_Megaman/Robo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Projeto_Megaman
+{
+    public class Robo
+    {
+       
+        public int Vida { get; protected set; }
+
+        
+        protected virtual int Ataque
+        {
+            get
+            {
+                return 5;
+            }
+        }
+
+        protected virtual int Defesa
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public Robo()
+        {
+            Vida = 100;
+        }
+
+        public virtual void Atacar(Robo robo)
+        {
+            robo.ReceberAtaque(this.Ataque);
+        }
+
+        public virtual void ReceberAtaque(int ataque)
+        {
+            int dano = ataque - this.Defesa;
+            this.Vida -= dano;
+        }
+
+    }
+}
