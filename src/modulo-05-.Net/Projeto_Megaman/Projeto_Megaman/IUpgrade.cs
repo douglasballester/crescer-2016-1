e69e0c78433d15c72bdd00426808bf1992894215
Douglas Ballester <douglas.ballester@cwi.com>
2016-05-08 01:18:41 -0300

    feat(IUpgrade): adicionar interface ao projeto megaman

---
+++ src/modulo-05-.Net/Projeto_Megaman/Projeto_Megaman/IUpgrade.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Megaman
+{
+    public interface IUpgrade
+    {
+        int UpgradeAtaque { get; }
+        int UpgradeDefesa { get; }
+    }
+}
