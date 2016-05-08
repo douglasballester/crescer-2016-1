26ec99112f6cccdaab7506485241997468f8bc9b
Douglas Ballester <douglas.ballester@cwi.com>
2016-05-08 02:07:55 -0300

    Refactor(Robo): melhorar método ReceberAtaque

--- src/modulo-05-.Net/Projeto_Megaman/Projeto_Megaman/Robo.cs
+++ src/modulo-05-.Net/Projeto_Megaman/Projeto_Megaman/Robo.cs
@@ -11,27 +11,18 @@ namespace Projeto_Megaman
     {
        
         public int Vida { get; protected set; }
+        protected List<IUpgrade> Upgrade = new List<IUpgrade>();
+        
+        protected virtual int Ataque{ get; set;}
+
+        protected virtual int Defesa { get; set; }
 
         
-        protected virtual int Ataque
-        {
-            get
-            {
-                return 5;
-            }
-        }
-
-        protected virtual int Defesa
-        {
-            get
-            {
-                return 0;
-            }
-        }
 
         public Robo()
         {
             Vida = 100;
+            Ataque = 5;
         }
 
         public virtual void Atacar(Robo robo)
@@ -42,7 +33,25 @@ namespace Projeto_Megaman
         public virtual void ReceberAtaque(int ataque)
         {
             int dano = ataque - this.Defesa;
-            this.Vida -= dano;
+            if (dano > 0)
+            {
+                this.Vida -= dano;
+            }
+        }
+
+        public virtual void EquiparUpgrade(IUpgrade upgrade)
+        {
+            if (Upgrade.Count < 3)
+            {
+                Upgrade.Add(upgrade);
+                Ataque += upgrade.UpgradeAtaque;
+                Defesa += upgrade.UpgradeDefesa;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + this.GetType().Name + ", Vida: " + this.Vida + ", Ataque: " + this.Ataque + "Defesa: " + this.Defesa;
         }
 
     }
