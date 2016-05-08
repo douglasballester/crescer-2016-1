68d82e8d1e83e9b6740d3986f4a3c07aaace202e
Douglas Ballester <douglas.ballester@cwi.com>
2016-05-08 01:23:12 -0300

    Fix(Protoman): modificar metodo receberAtaque para funcionar corretamente Test(ProtomanTest): adicionar cenarios de teste

--- src/modulo-05-.Net/Projeto_Megaman/Projeto_Megaman/Protoman.cs
+++ src/modulo-05-.Net/Projeto_Megaman/Projeto_Megaman/Protoman.cs
@@ -8,17 +8,20 @@ namespace Projeto_Megaman
 {
     public class Protoman : Robo
     {
-        
-        public Protoman()
+        private int contMorte = 0;
+        private int _defesa = 2;
+        private int _ataque = 5;
+
+
+        public Protoman() : base()
         {
-           
         }
 
         protected override int Ataque
         {
             get
             {
-                return Ataque;
+                return _ataque;
             }
         }
 
@@ -26,25 +29,30 @@ namespace Projeto_Megaman
         {
             get
             {
-                return 2;
+                return _defesa;
             }
-        }
-
-        public override void Atacar(Robo robo)
-        {
-            base.Atacar(robo);
         }
 
         public override void ReceberAtaque(int ataque)
         {
             base.ReceberAtaque(ataque);
-            int contMorte = 0;
-            if(Vida <= 0 && contMorte == 0)
+
+            if ((Vida <= 0) && (contMorte == 0))
             {
                 Vida = 20;
                 contMorte = 1;
+                _ataque += 2;
             }
         }
 
+        public override void EquiparUpgrade(IUpgrade upgrade)
+        {
+            if (Upgrade.Count < 3)
+            {
+                Upgrade.Add(upgrade);
+                _ataque += upgrade.UpgradeAtaque;
+                _defesa += upgrade.UpgradeDefesa;
+            }
+        }
     }
 }
---
+++ src/modulo-05-.Net/Projeto_Megaman/Projeto_Megaman_Test/Projeto_Megaman_Test/ProtomanTest.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Projeto_Megaman;
+
+namespace Projeto_Megaman_Test
+{
+    [TestClass]
+    public class ProtomanTest
+    {
+        [TestMethod]
+        public void CriarProtoman()
+        {
+            Robo protoman = new Protoman();
+
+            Assert.AreEqual(100, protoman.Vida);
+        }
+
+
+        [TestMethod]
+        public void ProtomanAtacaBot()
+        {
+            Robo protoman = new Protoman();
+            Robo bot = new Bot();
+
+            protoman.Atacar(bot);
+
+            Assert.AreEqual(95, bot.Vida);
+        }
+
+        [TestMethod]
+        public void ProtomanAtacaMegaman()
+        {
+            Robo protoman = new Protoman();
+            Robo megaman = new Megaman();
+
+            protoman.Atacar(megaman);
+
+            Assert.AreEqual(95, megaman.Vida);
+        }
+
+        [TestMethod]
+        public void ProtomanMortoVivoCom7DeAtaqueAtacaMegaman()
+        {
+            Robo protoman = new Protoman();
+            Robo megaman = new Megaman();
+
+            protoman.ReceberAtaque(102);
+            protoman.Atacar(megaman);
+
+            Assert.AreEqual(93, megaman.Vida);
+        }
+
+        [TestMethod]
+        public void ProtomanRessucita1VezCom20VidaE7DeAtaque()
+        {
+            Robo protoman = new Protoman();
+            
+            protoman.ReceberAtaque(102);
+
+            Assert.AreEqual(20, protoman.Vida);
+        }
+
+        [TestMethod]
+        public void ProtomanTentaRessucitar2VezesCom20VidaE7DeAtaque()
+        {
+            Robo protoman = new Protoman();
+
+            protoman.ReceberAtaque(102);
+            protoman.ReceberAtaque(22);
+
+            Assert.AreEqual(0, protoman.Vida);
+        }
+
+        [TestMethod]
+        public void ProtomanToString()
+        {
+            Robo protoman = new Protoman();
+
+            protoman.ToString();
+
+            Assert.AreEqual("Nome: " + "Protoman" + " Vida: " + 100 + ", Ataque: " + 5 + "Defesa: " + 2, protoman.ToString());
+        }
+
+        [TestMethod]
+        public void ProtomanCom1UpgradeDeAtaque()
+        {
+            Robo protoman = new Protoman();
+            Robo bot = new Bot();
+            CanhaoDePlasma upgrade = new CanhaoDePlasma();
+            protoman.EquiparUpgrade(upgrade);
+            protoman.Atacar(bot);
+           
+            Assert.AreEqual(93, bot.Vida);
+        }
+
+        [TestMethod]
+        public void ProtomanCom2UpgradeDeAtaqueAposTerMorrido1Vez()
+        {
+            Robo protoman = new Protoman();
+            Robo bot = new Bot();
+            CanhaoDePlasma upgrade = new CanhaoDePlasma();
+
+            protoman.ReceberAtaque(102);
+            protoman.EquiparUpgrade(upgrade);
+            protoman.EquiparUpgrade(upgrade);
+           
+
+            protoman.Atacar(bot);
+
+            Assert.AreEqual(89, bot.Vida);
+            Assert.AreEqual(20, protoman.Vida);
+        }
+
+        [TestMethod]
+        public void ProtomanCom1UpgradeDeCadaTipo()
+        {
+            Robo protoman = new Protoman();
+            Robo bot = new Bot();
+            BotasSuperVelocidade upgrade3 = new BotasSuperVelocidade();
+            EscudoEnergia upgrade2 = new EscudoEnergia();
+            CanhaoDePlasma upgrade = new CanhaoDePlasma();
+
+            protoman.EquiparUpgrade(upgrade);
+            protoman.EquiparUpgrade(upgrade2);
+            protoman.EquiparUpgrade(upgrade3);
+
+            protoman.Atacar(bot);
+            bot.Atacar(protoman);
+
+            Assert.AreEqual(100, protoman.Vida);
+            Assert.AreEqual(92, bot.Vida);
+        }
+
+        [TestMethod]
+        public void ProtomanCom1UpgradeDeCadaTipoRessucitad()
+        {
+            Robo protoman = new Protoman();
+            Robo bot = new Bot();
+            BotasSuperVelocidade upgrade3 = new BotasSuperVelocidade();
+            EscudoEnergia upgrade2 = new EscudoEnergia();
+            CanhaoDePlasma upgrade = new CanhaoDePlasma();
+
+            protoman.ReceberAtaque(200);
+            protoman.EquiparUpgrade(upgrade);
+            protoman.EquiparUpgrade(upgrade2);
+            protoman.EquiparUpgrade(upgrade3);
+
+            protoman.Atacar(bot);
+            bot.Atacar(protoman);
+
+            Assert.AreEqual(20, protoman.Vida);
+            Assert.AreEqual(90, bot.Vida);
+        }
+    }
+}
