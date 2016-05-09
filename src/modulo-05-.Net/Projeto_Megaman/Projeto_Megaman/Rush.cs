dd6aa21afa1c38b14328c7f33090e75c47eb6ff0
Douglas Ballester <douglas.ballester@cwi.com>
2016-05-09 02:31:20 -0300

    feat(Rush): Criar classe rush Test(RushTest): adicionar cenarios de teste

---
+++ src/modulo-05-.Net/Projeto_Megaman/Projeto_Megaman/Rush.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Megaman
+{
+    public class Rush : Robo, IUpgrade
+    {
+
+        public Rush(Chip chip) : this()
+        {
+           
+        }
+
+        public Rush() : base()
+        {
+            this.Ataque = 4;
+            this.Defesa = 3;
+            this.Upgrade = new List<IUpgrade>();
+        }
+
+        public int UpgradeAtaque
+        {
+            get
+            {
+                return Ataque;
+            }
+        }
+
+        public int UpgradeDefesa
+        {
+            get
+            {
+                return Defesa;
+            }
+        }
+
+        public override void EquiparUpgrade(IUpgrade upgrade)
+        {
+            if (Upgrade.Count < 2)
+            {
+                Upgrade.Add(upgrade);
+                Ataque += upgrade.UpgradeAtaque;
+                Defesa += upgrade.UpgradeDefesa;
+            }
+        }
+
+        public override void Atacar(Robo robo)
+        {
+            if(robo.GetType().Name != "Megaman")
+            {
+                robo.ReceberAtaque(Ataque);
+            }
+        }
+    }
+}
---
+++ src/modulo-05-.Net/Projeto_Megaman/Projeto_Megaman_Test/Projeto_Megaman_Test/RushTest.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Projeto_Megaman;
+
+namespace Projeto_Megaman_Test
+{
+    
+    [TestClass]
+    public class RushTest
+    {
+        [TestMethod]
+        public void CriarRush()
+        {
+            Robo rush = new Rush();
+
+            Assert.AreEqual(100, rush.Vida);
+        }
+
+        [TestMethod]
+        public void AdicionarUpgradeDeAtaque()
+        {
+            Robo rush = new Rush();
+            Robo bot = new Bot();
+
+            rush.EquiparUpgrade(new CanhaoDePlasma());
+            rush.Atacar(bot);
+            Assert.AreEqual(94, bot.Vida);
+            Assert.AreEqual(100, rush.Vida);
+        }
+
+        [TestMethod]
+        public void Adicionar3UpgradesNoRush()
+        {
+            Robo rush = new Rush();
+            Robo bot = new Bot();
+
+            rush.EquiparUpgrade(new CanhaoDePlasma());
+            rush.EquiparUpgrade(new CanhaoDePlasma());
+            rush.EquiparUpgrade(new CanhaoDePlasma());
+
+            rush.Atacar(bot);
+            Assert.AreEqual(92, bot.Vida);
+        }
+
+        [TestMethod]
+        public void RushAdicionaRushComoUpgrade()
+        {
+            Robo rush = new Rush();
+            Robo bot = new Bot();
+
+            rush.EquiparUpgrade(new Rush());
+            bot.EquiparUpgrade(new Rush());
+
+            rush.Atacar(bot);
+            bot.Atacar(rush);
+            Assert.AreEqual(95, bot.Vida);
+            Assert.AreEqual(97, rush.Vida);
+        }
+
+        [TestMethod]
+        public void BotAdicionaRushComUpgradeRushComoUpgrade()
+        {
+            Rush rush = new Rush();
+            Robo bot = new Bot();
+
+            rush.EquiparUpgrade(new Rush());
+
+            bot.EquiparUpgrade(rush);
+
+            rush.Atacar(bot);
+            bot.Atacar(rush);
+            Assert.AreEqual(98, bot.Vida);
+            Assert.AreEqual(93, rush.Vida);
+        }
+
+        [TestMethod]
+        public void RushAtacaMegaman()
+        {
+            Robo rush = new Rush();
+            Robo megaman = new Megaman();
+
+            rush.Atacar(megaman);
+            Assert.AreEqual(100, megaman.Vida);
+        }
+    }
+}
