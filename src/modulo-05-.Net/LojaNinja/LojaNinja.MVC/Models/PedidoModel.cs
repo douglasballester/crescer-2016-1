9cb516bbbbc96f30bbbf0d8942965911a1d29f72
Douglas Ballester <douglas.ballester@cwi.com>
2016-05-15 23:55:50 -0300

    feat(LojaNinja): Adicionar opções de deletar e filtrar

---
+++ src/modulo-05-.Net/LojaNinja/LojaNinja.MVC/Models/PedidoModel.cs
@@ -0,0 +1,43 @@
+﻿using LojaNinja.Dominio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LojaNinja.MVC.Models
+{
+    public class PedidoModel
+    {
+        public int? Id { get; set; }
+
+        [Required]
+        public DateTime DataEntrega { get; set; }
+
+        [Display(Name = "Nome do produto ")]
+        [Required(ErrorMessage = "Informe o nome do produto.")]
+        public string NomeProduto { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d+.\d{0,2}$", ErrorMessage = "Valor da venda deverá ser informado.")]
+        public decimal Valor { get; set; }
+
+        [Required]
+        public TipoPagamento TipoPagamento { get; set; }
+
+        [Display(Name = "Nome ")]
+        [Required(ErrorMessage = "Informe seu nome.")]
+        public string NomeCliente { get; set; }
+
+        [Display(Name = "Cidade ")]
+        [Required(ErrorMessage = "Informe a cidade.")]
+        public string Cidade { get; set; }
+
+        [Display(Name = "Estado ")]
+        [StringLength(2, ErrorMessage = "Estado deverá ter 2 caracteres")]
+        [Required(ErrorMessage = "Informe o estado.")]
+        public string Estado { get; set; }
+
+    }
+}
