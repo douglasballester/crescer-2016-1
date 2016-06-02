85a0f0429b2cc94128fe72ab732e7c82503297ed
Douglas Valle Ballester <douglas.ballester@cwinet.local>
2016-06-02 17:35:19 -0300

    feat(megaSena): adicionar exercicio da megaSena(em andamento)

--- src/modulo-07-banco-parte2/ExerciciosAula02-1.sql
+++ src/modulo-07-banco-parte2/ExerciciosAula02-1.sql
@@ -16,3 +16,32 @@ BEGIN
   DBMS_OUTPUT.PUT_LINE('Nome: ' || vNome);
   DBMS_OUTPUT.PUT_LINE('Cidade: ' || vCidade);
 END;
+
+DECLARE
+vNomeCliente Cliente.Nome%Type;
+vNomeCidade Cidade.Nome%Type;
+vPrimeiraCompra pedido.datapedido%type;
+vUltimaCompra pedido.datapedido%type;
+vValor pedido.valorpedido%type;
+Begin
+
+select cli.nome, cid.nome, Min(ped.datapedido), Max(ped.Datapedido), sum(ped.valor)
+into vNomeCliente, vNomeCidade
+from cliente cli
+  left join cidade cid on cid.IDcidade = cli.idcidade
+  left join pedido ped on ped.idcliente = cli.idcliente
+  where cli.idcliente = &pidcliente
+  group by cli.nome, cid.nome;
+  
+END;
+
+-- PROCEDURE
+
+CREATE PROCEDURE recebeId(IDpedido in integer) as
+BEGIN
+
+  update PEDIDO
+    set pedido.ValorPedido = pedidoitem.quantidade * produto.precovenda
+  where IDPEDIDO = IDpedido;        
+        
+END;
